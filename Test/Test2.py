53d4d745b377099a4c617dfe800d24f5bd0ab328
Tanawin Intarattanaphol <tanawin.i@ku.th>
2025-08-19 14:55:48 +0700

    .

---
+++ Test/Test2.py
@@ -0,0 +1,13 @@
+def weather_suggestion(temp):
+    if temp > 35:
+        return"very hot, stay indoor"
+    elif 25 <= temp <= 35:
+        return"nice weather"
+    elif 20 <= temp <= 24:
+        return"quite cool"
+    else:  #temp < 20
+        return"cool,wear jacket"
+    #ทดสอบฟังก์ชัน
+    test_temps= [38,30,22,18]
+    for t in test_temp:
+        print(f" Temp{t}"c {weather_suggestion(t)}")
