b986f3e765c0e86a14566a455b278934898b2230
Somchai Jaidee <somchai@email.com>
2026-02-05 20:28:06 +0700

    .

--- Stock/php/config.php
+++ Stock/php/config.php
@@ -1,7 +1,7 @@
 <?php
 $host = "localhost";
-$user = "std6730202050"; //ชื่อ username ที่จารให้
-$pass = ""; //รหัสที่จากให้
+$user = "std6730202629"; //ชื่อ username ที่จารให้
+$pass = "X7m^vT2r"; //รหัสที่จากให้
 $db   = "it_std6730202629"; //ชื่อ database
 
 $conn = mysqli_connect($host, $user, $pass, $db);
